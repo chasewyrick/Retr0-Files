51aee4adb705cb2bbc802bb316bd80ac5e7373fa
Zer0 <pkmnx7802@gmail.com>
2017-09-28 12:04:01 -0600

    Add files via upload

--- Kitties/Kitties/Program.cs
+++ Kitties/Kitties/Program.cs
@@ -13,7 +13,7 @@ namespace Kitties
             Console.Write("How many kitties do you have?: ");
             int kitties = int.Parse(Console.ReadLine());
 
-            Console.Write("Each person gets " + (kitties / 4) + " net kitties.");
+            Console.Write("Each person gets " + (kitties / 3) + " net kitties. There is " + (kitties % 3) + " going to the dog pound.");
 
             Console.ReadLine();
         }
