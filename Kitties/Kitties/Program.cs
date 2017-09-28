e2a313072410da3a4b83f279b63dd276aa69e032
Zer0 <pkmnx7802@gmail.com>
2017-09-28 11:47:09 -0600

    Add files via upload

---
+++ Kitties/Kitties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many kitties do you have?: ");
+            int kitties = int.Parse(Console.ReadLine());
+
+            Console.Write("Each person gets " + (kitties / 4) + " net kitties.");
+
+            Console.ReadLine();
+        }
+    }
+}
