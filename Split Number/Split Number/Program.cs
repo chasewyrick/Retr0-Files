87d0badc7e258dc505aba27c36b3c87669e473d5
Zer0 <pkmnx7802@gmail.com>
2017-10-02 10:47:32 -0600

    Add files via upload

---
+++ Split Number/Split Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Number: ");
+            int tard = int.Parse(Console.ReadLine());
+
+            int bigger = tard / 100;
+            int weird = tard % 100;
+            int medium = weird / 10;
+            int small = weird % 10;
+
+            Console.WriteLine(bigger + medium + small);
+
+
+            Console.ReadLine();
+        }
+    }
+}
