b21130c5cf58db15f8e19c3928fac48cc2524483
Zer0 <pkmnx7802@gmail.com>
2017-10-02 14:26:54 -0600

    Add files via upload

---
+++ Joe ClockWatcher/Joe ClockWatcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joe_ClockWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Current Time: ");
+            int Time = int.Parse(Console.ReadLine());
+
+            int shiftleft = 1600 - Time;
+            int hour = shiftleft / 100;
+            int minutes = shiftleft % 100;
+
+            Console.Write("Hours: " + hour + " Minutes: " + minutes);
+
+
+            Console.Read();
+        }
+    }
+}
