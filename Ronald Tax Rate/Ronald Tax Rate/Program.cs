74c612bb4e9c88d3cf2c70ba6182e8f59079be65
Zer0 <pkmnx7802@gmail.com>
2017-10-04 11:56:00 -0600

    Add files via upload

---
+++ Ronald Tax Rate/Ronald Tax Rate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ronald_Tax_Rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Earned Net Kitties: ");
+            int total_kit = int.Parse(Console.ReadLine());
+            Console.Write("Enter Tax Rate: ");
+            double taxRate = double.Parse(Console.ReadLine());
+
+            int netKit = (int)(total_kit * taxRate);
+
+            Console.Write("The Net Kitties are: " + netKit);
+
+            Console.Read();
+        }
+    }
+}
