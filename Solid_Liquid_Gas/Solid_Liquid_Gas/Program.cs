d04da7b028b704dbd19e6a430a64b1ad72fdfec2
6ilent <pkmnx7802@gmail.com>
2017-12-13 11:32:35 -0700

    Add files via upload

---
+++ Solid_Liquid_Gas/Solid_Liquid_Gas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Liquid_Gas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter current temperature of water in degress celsius: ");
+            int temp = int.Parse(Console.ReadLine());
+
+            if (temp <= 0)
+            {
+                Console.Write("It is a solid.");
+            }
+            else if (temp >= 100)
+            {
+                Console.Write("It is a gas.");
+            }
+            else
+            {
+                Console.Write("It is a liquid.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
